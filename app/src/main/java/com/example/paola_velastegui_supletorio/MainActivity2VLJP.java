4c2d2292d691732c7de00d8d2f6150fcca46a934
PaolaVelastegui <jvelastegui8895@uta.edu.ec>
2022-02-16 20:52:09 -0500

    commit 8h45

--- app/src/main/java/com/example/paola_velastegui_supletorio/MainActivity2VLJP.java
+++ app/src/main/java/com/example/paola_velastegui_supletorio/MainActivity2VLJP.java
@@ -1,14 +1,56 @@
 package com.example.paola_velastegui_supletorio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity2VLJP extends AppCompatActivity {
+    private MainActivity2VLJPBinding binding;
+    private ImageView añadirimagen;
+    private  ImageView reportes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activity2_vljp);
+
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        añadirimagen = findViewById(R.id.imageView_add);
+        reportes = findViewById(R.id.reportesid);
+        reportes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, Reportesfinal.class);
+                startActivity(intent);
+            }
+        });
+        añadirimagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nuevatarea();
+            }
+        });
+
     }
+
+
+
+    private void nuevatarea() {
+        Intent intent = new Intent(MainActivity2.this, AgregarTareas.class);
+        startActivity(intent);
+    }
+
 }
